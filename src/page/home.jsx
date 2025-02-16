12d0bd7bbd0dc6c70a98aafd9f36939c03bfa969
hitesh <hiteshtripathi91@gmail.com>
2025-02-16 23:54:28 +0530

    homr page

---
+++ src/page/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className='home'><Link to='shop'><button  className='center'>SHOP-NOW</button></Link></div>
+  )
+}
+
+export default Home
